bbe42043de3919fd392490017d7003ca0dbea16a
Michael Bruns <97768204+michaelbruns1@users.noreply.github.com>
2023-03-24 06:29:16 +0000

    Added the replace awesome word function and tests.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-export default () => "hello universe"
+export default (content, words) => (
+  content.replace(new RegExp(words.join('|'), 'ig'),
+    '***'
+  ))
+
+//create the module that will filter a sentence and replace the blacklisted words with asterisks
+/**  
+var filterWords = function(sentence, blackList) {
+  var words = sentence.split(" ");
+  var filteredWords = [];
+  for (var i = 0; i < words.length; i++) {
+    if (blackList.indexOf(words[i]) === -1) {
+      filteredWords.push(words[i]);
+    } else {
+      filteredWords.push("***".repeat(words[i].length));
+    }
+  }
+  return filteredWords.join(" ");
+};
+
+module.exports = filterWords;
+*/
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,16 @@
 import awesomeWords from '.'
 
-test('says hello world', () => (
-  expect(awesomeWords()).toBe('hello universe')
+test('replaces blacklisted words with asterisks', () => (
+  expect(awesomeWords(
+    'The name of the NX will be the Nintendo Switch',
+    ['switch']
+  )).toBe('The name of the NX will be the Nintendo ***')
+))
+
+
+test('replaces multiple instances of blacklisted words', () => (
+  expect(awesomeWords(
+    'The name of the NX will be the Nintendo Switch. The Switch will be awesome.',
+    ['switch']
+  )).toBe('The name of the NX will be the Nintendo ***. The *** will be awesome.')
 ))
